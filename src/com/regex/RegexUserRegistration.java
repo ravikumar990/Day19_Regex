1791b8958e40ec4223bff972efef4a74b4de507d
ravi <ravi@gmail.com>
2021-11-12 23:29:03 +0530

    Code for check the first name

---
+++ src/com/regex/RegexUserRegistration.java
@@ -0,0 +1,11 @@
+package com.regex;
+
+public class RegexUserRegistration {
+	public static void main(String args[]) {
+		// created user info object
+		UserInfo userInfo = new UserInfo();
+
+		// calling method in UserInfo class
+		userInfo.userFirstName();
+	}
+}
---
+++ src/com/regex/UserInfo.java
@@ -0,0 +1,18 @@
+package com.regex;
+
+import java.util.Scanner;
+
+public class UserInfo {
+	Scanner sc = new Scanner(System.in);
+	ValidateUserInfo validateInfo = new ValidateUserInfo();
+
+	public void userFirstName() {
+		//taking user input
+		System.out.println("Enter Your First Name : ");
+		String firstName = sc.next();
+		
+		//call firstName method in Validate class
+		validateInfo.firstName(firstName);
+		sc.close();
+	}
+}
