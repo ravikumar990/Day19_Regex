a9d53c718de8566b16b9027f71641b268a84a734
ravi <ravi@gmail.com>
2021-11-13 03:13:05 +0530

    Added happy and sad

--- test/com/regex/ValidateUserInfoTest.java
+++ test/com/regex/ValidateUserInfoTest.java
@@ -9,71 +9,71 @@ public class ValidateUserInfoTest {
 
 	@Test
 	public void firstNameValid() {
-		boolean expectedVal = true;
-		boolean actualResult = validateInfo.firstName("Ravi");
+		String expectedVal = "Happy";
+		String actualResult = validateInfo.firstName("Ravi");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 
 	@Test
 	public void firstNameInValid() {
-		boolean expectedVal = false;
-		boolean actualResult = validateInfo.firstName("ravi");
+		String expectedVal = "Sad";
+		String actualResult = validateInfo.firstName("ravi");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 
 	@Test
 	public void lastNameValid() {
-		boolean expectedVal = true;
-		boolean actualResult = validateInfo.lastName("Kumar");
+		String expectedVal = "Happy";
+		String actualResult = validateInfo.lastName("Kumar");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 
 	@Test
 	public void lastNameInValid() {
-		boolean expectedVal = false;
-		boolean actualResult = validateInfo.lastName("kumar");
+		String expectedVal = "Sad";
+		String actualResult = validateInfo.lastName("kumar");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 
 	@Test
 	public void emailIdValid() {
-		boolean expectedVal = true;
-		boolean actualResult = validateInfo.emailId("ravi.kumar@sample.com");
+		String expectedVal = "Happy";
+		String actualResult = validateInfo.emailId("ravi.kumar@sample.com");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 
 	@Test
 	public void emailIdInValid() {
-		boolean expectedVal = false;
-		boolean actualResult = validateInfo.emailId("ravi.kumar.co.in");
+		String expectedVal = "Sad";
+		String actualResult = validateInfo.emailId("ravi.kumar.co.in");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 
 	@Test
 	public void mobNumValid() {
-		boolean expectedVal = true;
-		boolean actualResult = validateInfo.mobNum("54-5465322154");
+		String expectedVal = "Happy";
+		String actualResult = validateInfo.mobNum("54-5465322154");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 
 	@Test
 	public void mobNumInValid() {
-		boolean expectedVal = false;
-		boolean actualResult = validateInfo.mobNum("54-55465322154");
+		String expectedVal = "Sad";
+		String actualResult = validateInfo.mobNum("54-55465322154");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 
 	@Test
 	public void passwordValid() {
-		boolean expectedVal = true;
-		boolean actualResult = validateInfo.password("ab98cDE@&fg");
+		String expectedVal = "Happy";
+		String actualResult = validateInfo.password("ab98cDE@&fg");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 
 	@Test
 	public void passwordInValid() {
-		boolean expectedVal = false;
-		boolean actualResult = validateInfo.password("ab98cDEfg");
+		String expectedVal = "Sad";
+		String actualResult = validateInfo.password("ab98cDEfg");
 		Assert.assertEquals(expectedVal, actualResult);
 	}
 }
